d6584dacdaa0db090d70fc338b0d3c27089bf798
shiyinglive <shiyingli.li.57@gmail.com>
2026-04-13 18:29:22 -0500

    talkmap.ipynb updated
    package update; folder update

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,102 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Seattle, WA",
+    47.6038321,
+    -122.330062
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Chapel Hill, NC",
+    35.9131542,
+    -79.05578
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Virginia Tech, Blacksburg",
+    37.2219268,
+    -80.4272818
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "UNC-Chapel Hill, NC",
+    35.9050354,
+    -79.0477533
+  ],
+  [
+    "San Francisco, CA",
+    37.7879363,
+    -122.4075201
+  ],
+  [
+    "Newark, New Jersey",
+    40.735657,
+    -74.1723667
+  ],
+  [
+    "Tokyo, Japan",
+    35.6768601,
+    139.7638947
+  ],
+  [
+    "Lincoln, NE",
+    40.8088861,
+    -96.7077751
+  ],
+  [
+    "Durham, NC",
+    35.996653,
+    -78.9018053
+  ],
+  [
+    "UNC-Chapel Hill",
+    35.9050354,
+    -79.0477533
+  ],
+  [
+    "Nashville, Tennessee",
+    36.1622767,
+    -86.7742984
+  ],
+  [
+    "Atlanta, Georgia",
+    33.7544657,
+    -84.3898151
+  ],
+  [
+    "Baltimore, MD",
+    39.2908816,
+    -76.610759
+  ],
+  [
+    "NC State University",
+    35.7718497,
+    -78.674087
+  ],
+  [
+    "New York, NY",
+    40.7127281,
+    -74.0060152
+  ],
+  [
+    "Nashville, TN, USA",
+    36.1622767,
+    -86.7742984
+  ],
+  [
+    "Alexandria, VA",
+    38.8051095,
+    -77.0470229
+  ],
+  [
+    "Java Center, NY",
+    42.6500613,
+    -78.3858509
+  ],
+  [
+    "Salt Lake City, UT",
+    40.7596198,
+    -111.886797
+  ],
+  [
+    "Brown University, Providence, RI",
+    41.8186395,
+    -71.4088009
   ]
 ];
